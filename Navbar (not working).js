ce545990366df7ec3aa067df377ac23b09711427
Noklle <106414166+Noklle@users.noreply.github.com>
2024-11-26 14:52:43 +1100

    added navbar

---
+++ Navbar (not working).js
@@ -0,0 +1,13 @@
+function navbar())
+{
+    var navbar = ""<nav id="main_nav">
+        <ul>
+            <li><a href="/Login/">Login</a></li>
+            <li><a href="/Signup/">Signup</a></li>
+            <li><a href="/Profile/">Profile</a></li>
+        </ul>
+    </nav>";
+    $('#main nav').html(navbar);
+    }
+// <header id="main nav"></header>
+// https://stackoverflow.com/questions/47106985/use-same-navbar-for-multiple-html-files
