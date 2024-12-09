70deee56930dcc61122fe242e59e4487dc1238f6
PrestigiousCrown <kennethw1702@gmail.com>
2024-12-09 15:40:08 +1100

    Update server.js

--- GameReviewWeb/server.js
+++ GameReviewWeb/server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'Public')));
 
 // Serve the search.html file when accessing the root URL
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Public', 'search.html'));
+    res.sendFile(path.join(__dirname, 'Public', 'Home.html'));
 });
 
 // Database path
